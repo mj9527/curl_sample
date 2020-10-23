b41856709130eb4e0a5ae445d5066ec72b45ccc3
mjzheng <mjzheng@tencent.com>
2020-10-23 11:01:43 +0800

    add files

---
+++ cmake-build-debug/CMakeFiles/curl_sample.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/mjzheng/Documents/mj_git/curl_sample/main.cpp" "/Users/mjzheng/Documents/mj_git/curl_sample/cmake-build-debug/CMakeFiles/curl_sample.dir/main.cpp.o"
+  "/Users/mjzheng/Documents/mj_git/curl_sample/mycurl/HttpImpl.cpp" "/Users/mjzheng/Documents/mj_git/curl_sample/cmake-build-debug/CMakeFiles/curl_sample.dir/mycurl/HttpImpl.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/curl_sample.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/curl_sample.dir/main.cpp.o"
+  "CMakeFiles/curl_sample.dir/mycurl/HttpImpl.cpp.o"
+  "curl_sample"
+  "curl_sample.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/curl_sample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
